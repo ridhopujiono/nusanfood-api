bfe98fb8881a15d03cc24e0191af846fc06a22e1
Ridho Pujiono <ridhopujiono2@gmail.com>
2026-01-02 20:10:58 +0700

    feat: add user repository with FindByEmail method!

---
+++ internal/modules/user/repository.go
@@ -0,0 +1,22 @@
+package user
+
+import (
+	"errors"
+
+	"github.com/ridhopujiono/nusanfood-api/internal/database"
+	"gorm.io/gorm"
+)
+
+func FindByEmail(email string) (*User, error) {
+	var user User
+
+	err := database.DB.
+		Where("email = ?", email).
+		First(&user).Error
+
+	if errors.Is(err, gorm.ErrRecordNotFound) {
+		return nil, errors.New("user not found")
+	}
+
+	return &user, err
+}
